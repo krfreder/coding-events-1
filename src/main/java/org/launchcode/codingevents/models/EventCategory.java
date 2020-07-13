3269d5629407a522d7c1a307b488cddc0f95c80c
Kyleah <krfrederic@gmail.com>
2020-07-13 10:17:39 -0500

    one-to-many EventCategory and Event

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,13 +1,19 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
  */
 @Entity
 public class EventCategory extends AbstractEntity {
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
@@ -25,6 +31,8 @@ public class EventCategory extends AbstractEntity {
     public void setName(String name) {
         this.name = name;
     }
+
+    public List<Event> getEvents() { return events; }
 
     @Override
     public String toString() {
